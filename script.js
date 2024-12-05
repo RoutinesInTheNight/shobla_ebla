8750775a9ad33d5baee708df0890e4849df99912
RoutinesInTheNight <yeahup@icloud.com>
2024-12-05 05:12:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const buttonElement = document.getElementById("incrementButton");
 // Функция для загрузки текущего значения счётчика
 async function fetchCounter() {
     try {
-        const response = await fetch("https://9dd60039-97a1-404b-ba3c-5385bb3469e9.tunnel4.com/counter");
+        const response = await fetch("https://cdc9aaed-fcbc-40d5-b3db-e71f753186b3.tunnel4.com/counter");
         if (!response.ok) throw new Error("Ошибка загрузки числа");
         const data = await response.json();
         counterElement.textContent = data.value;
@@ -16,7 +16,7 @@ async function fetchCounter() {
 // Функция для увеличения счётчика
 async function incrementCounter() {
     try {
-        const response = await fetch("https://9dd60039-97a1-404b-ba3c-5385bb3469e9.tunnel4.com/increment", { method: "POST" });
+        const response = await fetch("https://cdc9aaed-fcbc-40d5-b3db-e71f753186b3.tunnel4.com/increment", { method: "POST" });
         if (!response.ok) throw new Error("Ошибка обновления числа");
         const data = await response.json();
         counterElement.textContent = data.value;
